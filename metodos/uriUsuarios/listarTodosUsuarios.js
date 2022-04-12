31f0179ad6f17a4054e80dca32cbc4af83733af5
jrbigmon <junioterriaga0@gmail.com>
2022-04-12 16:56:14 -0300

    Alterado a resposta do terminal ao acessar a URI usuarios

--- metodos/uriUsuarios/listarTodosUsuarios.js
+++ metodos/uriUsuarios/listarTodosUsuarios.js
@@ -1,6 +1,6 @@
 const listarTodosUsuarios = (app, lista) => {
     app.get('/usuarios', (req, res) => {
-    console.log('Cliente na pag de contato')
+    console.log('Client in User Page')
     return res.json(lista)
 })}
 module.exports = listarTodosUsuarios;
